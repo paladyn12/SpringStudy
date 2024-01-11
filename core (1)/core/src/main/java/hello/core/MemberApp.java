628e2ba408e646c34cff0a8a6b64c807b3d3e329
paladyn12 <paladyn12@naver.com>
2024-01-11 21:45:38 +0900

    240111 SpringStudy

--- core (1)/core/src/main/java/hello/core/MemberApp.java
+++ core (1)/core/src/main/java/hello/core/MemberApp.java
@@ -12,7 +12,7 @@ public class MemberApp {
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Member findMember = memberService.findMember(1L);
+        Member findMember = memberService.findMember(2L);
 
         System.out.println("new member = " + member.getName());
         System.out.println("findMember = " + findMember.getName());
