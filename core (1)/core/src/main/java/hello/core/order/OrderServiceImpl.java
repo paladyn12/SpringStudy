50486d55f404002a05b1910ef86f9e35040800a5
paladyn12 <paladyn12@naver.com>
2024-01-11 22:34:22 +0900

    240111 SpringStudy

---
+++ core (1)/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package hello.core.order;
+
+public class OrderServiceImpl implements OrderService{
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        return null;
+    }
+}
