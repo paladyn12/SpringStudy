7093b420d77898dee4def2e8dac27872e512a13e
paladyn12 <paladyn12@naver.com>
2024-01-15 19:56:14 +0900

    240115 SpringStudy

--- core (1)/core/src/main/java/hello/core/AppConfig.java
+++ core (1)/core/src/main/java/hello/core/AppConfig.java
@@ -1,21 +1,38 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig { // 실제 동작에 필요한 구현 객체들을 생성, 생성자를 통해 주입(생성자 주입)
 
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+    @Bean
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     } // MemberServiceImpl 입장에서 의존관계를 외부에서 주입하는 듯 보임 >> 의존관계 주입
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
     }
 }
