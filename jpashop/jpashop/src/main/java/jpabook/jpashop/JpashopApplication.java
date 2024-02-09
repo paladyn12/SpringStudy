573d9ada282c3e2aa8d7d5f8d36b19074b70191f
paladyn12 <paladyn12@naver.com>
2024-02-09 21:45:36 +0900

    240209 SpringStudy

--- jpashop/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -9,7 +9,7 @@ public class JpashopApplication {
 	public static void main(String[] args) {
 
 		Hello hello = new Hello();
-		hello.setData("hello");
+		hello.setData("hi");
 		String data = hello.getData();
 		System.out.println("data = " + data);
 
