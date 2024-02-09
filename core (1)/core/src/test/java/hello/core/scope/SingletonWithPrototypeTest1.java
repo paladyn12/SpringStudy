3f75fe549f01f7269250762174ea4c38cafa8648
paladyn12 <paladyn12@naver.com>
2024-02-09 19:58:28 +0900

    240209 스프링 핵심 원리 기본편 최종

--- core (1)/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core (1)/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,11 +3,15 @@ package hello.core.scope;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -34,16 +38,20 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
         //프로토타입 빈은 싱글톤 빈이 생성될 때 할당, 이후 새로 생겨나지 않게됨, 문제 발생
     }
 
     @Scope("singleton")
-    @RequiredArgsConstructor
-    static class ClientBean{ //싱글톤 빈이 프로토타입 빈을 가짐, 프로토타입 빈이 새로 생기지 않음
-        private final PrototypeBean prototypeBean; //생성 시점에 주입
+    static class ClientBean{
+
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvicer;
+        //getObject를 호출하면 그 때서야 스프링 컨테이너에서 프로토타입 빈을 찾아 반환해줌
+        //찾아주는 기능만 제공, 대신 조회해주는 대리자 개념
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvicer.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
