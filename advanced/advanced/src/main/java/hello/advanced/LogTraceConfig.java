02d074ac3a588e5a388da6f16ebb44aeeedee2e0
Taewan Kim <paladyn12@naver.com>
2024-07-03 18:12:56 +0900

    240703 SpringStudy

--- advanced/advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ advanced/advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogTraceConfig {
-
     @Bean
     public LogTrace logTrace() {
         return new ThreadLocalLogTrace();
