9d84c061cac73d1977e84e92de0164cf7c8d927b
Taewan Kim <paladyn12@naver.com>
2024-07-08 16:14:11 +0900

    240708 SpringStudy

---
+++ advanced/proxy/src/main/java/hello/advanced/app/v1/OrderControllerV1.java
@@ -0,0 +1,31 @@
+package hello.advanced.app.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
+
+    @GetMapping("/v1/request")
+    public String request(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던져 주어야 함 (로그는 정상 흐름에 영향을 주면 안됨, 로그 때문에 예외가 사라지면 안됨)
+        }
+
+    }
+}
