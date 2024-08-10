2015b9596e0e6f82db16127331a5297cccebccff
Taewan Kim <paladyn12@naver.com>
2024-08-11 00:17:28 +0900

    240811 SpringStudy

--- advanced/proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
+++ advanced/proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -25,7 +25,6 @@ public class LogTraceAspect {
         try {
             String message = joinPoint.getSignature().toShortString();
             status = logTrace.begin(message);
-
             //실행 로직
             Object result = joinPoint.proceed();
 
