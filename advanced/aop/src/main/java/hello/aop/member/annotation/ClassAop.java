c1a886d7eaf3cccffb0e1fa8681ebfdc51906230
Taewan Kim <paladyn12@naver.com>
2024-08-13 00:22:38 +0900

    240813 SpringStudy

--- advanced/aop/src/main/java/hello/aop/member/annotation/ClassAop.java
+++ advanced/aop/src/main/java/hello/aop/member/annotation/ClassAop.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE) //클래스에 붙이는 애노테이션
+@Target(ElementType.TYPE) //Class에 붙이는 애노테이션
 @Retention(RetentionPolicy.RUNTIME) //런타임에 애노테이션이 살아있음
 public @interface ClassAop {
 }
--- advanced/aop/src/main/java/hello/aop/member/annotation/MethodAop.java
+++ advanced/aop/src/main/java/hello/aop/member/annotation/MethodAop.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD) //메서드에 붙일 수 있는 애노테이션
+@Target(ElementType.METHOD) //Method에 붙일 수 있는 애노테이션
 @Retention(RetentionPolicy.RUNTIME) //런타임에 애노테이션이 살아있음
 public @interface MethodAop {
     String value();
